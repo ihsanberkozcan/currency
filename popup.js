ee0de276b65ea101f76d2d0c3b6e596d6237407f
İhsan Berk Özcan <59116996+ihsanberkozcan@users.noreply.github.com>
2023-09-23 15:31:58 +0300

    initial commit

---
+++ popup.js
@@ -0,0 +1,16 @@
+const dolar = document.querySelector("#dolar");
+const euro = document.querySelector("#euro");
+const altin = document.querySelector("#altin");
+const bitcoin = document.querySelector("#bitcoin");
+
+
+const fetchCurrency = async () => {
+  const response = await fetch("https://api.genelpara.com/embed/doviz.json");
+  const currency = await response.json();
+  dolar.innerHTML = currency.USD.satis;
+  euro.innerHTML = currency.EUR.satis;
+  altin.innerHTML = currency.GA.satis;
+  bitcoin.innerHTML = currency.BTC.satis;
+};
+
+fetchCurrency();
