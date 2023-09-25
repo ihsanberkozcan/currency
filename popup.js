2d34b3e4a86e1cd4d1db063ea3f1eeac6cfdfd6a
İhsan Berk Özcan <59116996+ihsanberkozcan@users.noreply.github.com>
2023-09-25 15:00:18 +0300

    Turkish Lira and dollar sign

--- popup.js
+++ popup.js
@@ -3,14 +3,13 @@ const euro = document.querySelector("#euro");
 const altin = document.querySelector("#altin");
 const bitcoin = document.querySelector("#bitcoin");
 
-
 const fetchCurrency = async () => {
   const response = await fetch("https://api.genelpara.com/embed/doviz.json");
   const currency = await response.json();
-  dolar.innerHTML = currency.USD.satis;
-  euro.innerHTML = currency.EUR.satis;
-  altin.innerHTML = currency.GA.satis;
-  bitcoin.innerHTML = currency.BTC.satis;
+  dolar.innerHTML = currency.USD.satis + " ₺";
+  euro.innerHTML = currency.EUR.satis + " ₺";
+  altin.innerHTML = currency.GA.satis + " ₺";
+  bitcoin.innerHTML = "$ " + currency.BTC.satis;
 };
 
 fetchCurrency();
